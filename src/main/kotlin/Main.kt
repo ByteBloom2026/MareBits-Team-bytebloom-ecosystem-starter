9c7de2bab67a22d27c98ef835617cf285c56b2a6
IbtisamSafi <safiibtisam891@gmail.com>
2025-11-23 19:35:13 +0200

    add the two function parseMenteeRaw and parsePerformanceRaw

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ import java.io.File
 fun main() {
     println("ByteBloom Academy: Ecosystem Project Starter")
     println("✅ Project setup is correct and runnable.")
+    println("welcom the marebits team")
     val the_new_mentees=parseMenteeRaw()
     val the_new_performance= parsePerformanceRaw()
     println("$the_new_mentees")
