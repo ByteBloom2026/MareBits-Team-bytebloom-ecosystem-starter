a32b9ab44a74d1db2661706a31f92708a2d08762
ibtisam safi <safiibtisam891@gmail.com>
2025-12-09 13:50:55 +0200

    Fix typo in else statements in Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,19 +4,19 @@ fun main() {
     val theNewMentees = parseMenteeRaw()
     if (theNewMentees!= null) {
         println("totel menteess : {${theNewMentees.size}}")
-    } else{
+    } ese{
         println("no  find data on mentees file")
     }
     val theNewPerformances = parsePerformanceRaw()
     if (theNewPerformances!= null) {
         println("totel preformances : {${theNewPerformances.size}}")
-    } else{
+    } ese{
         println("no  find data on predormance file")
     }
     val parsedTeams = parseTeamData()
     if (parsedTeams!= null) {
         println("Total teams parsed : ${parsedTeams.size}")
-    } else{
+    } ese{
         println("No team data available for parsing .")
     }
 }
