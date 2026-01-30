5590dff88b65cc59490e14d87f8ed00d36e90388
IbtisamSafi <safiibtisam891@gmail.com>
2026-01-30 16:55:29 +0200

    add the fuature

--- src/main/kotlin/domain/model/Attendance.kt
+++ src/main/kotlin/domain/model/Attendance.kt
@@ -2,8 +2,4 @@ package domain.model
 data class Attendance(
     val menteeId : String ,
     val weeks: List<String>,
-    val states: AttendanceStates
-)
-enum class AttendanceStates{
-    LATE ,ABSENCE,PRESENT
-}
+    )
